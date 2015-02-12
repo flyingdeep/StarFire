09d6656b41df035095a4abb9e9682b0ed06ec3f1
flyingdeep <flyingdeep@163.com>
2015-02-12 18:42:25 +0800

    Update UI Appframework 3

--- codes/StarFire/framework/appframework-master/src/af.shim.js
+++ codes/StarFire/framework/appframework-master/src/af.shim.js
@@ -1,8 +1,8 @@
 /**
- * jq.appframework.js
- * @copyright Intel 2013
+ * af.shim.js
+ * @copyright Intel 2014
  * @author Ian Maffett
- * @description A plugin to allow jQuery developers to use App Framework UI
+ * @description jQuery helper functions for App Framework
  */
 /* jshint eqeqeq:false */
 (function($,window){
@@ -10,7 +10,7 @@
     jQuery.event.props.push("touches");
     jQuery.event.props.push("originalTouches");
     jQuery.event.props.push("changedTouches");
-    var nundefined, document = window.document,classCache = {},isWin8=(typeof(MSApp)==="object"),_jsonPID = 1;
+    var nundefined, document = window.document,classCache = {},isWin8=(typeof(MSApp)==="object");
 
     function classRE(name) {
         return name in classCache ? classCache[name] : (classCache[name] = new RegExp("(^|\\s)" + name + "(\\s|$)"));
@@ -26,12 +26,16 @@
             obj[obj.length++] = nodes[i];
     }
     $.extend($.fn,{
-        /* @param {String} attribute to get
-        * @param {String} value to set as
-        * @return {Object} an appframework object
-        * @title $().css(attribute,[value])
+        /**
+         * Get/Set vendor specific CSS
+         * Also set the vendor neutral version
+         * @param {String} attribute to get
+         * @param {String} value to set as
+         * @return {Object} an appframework object
+         * @title $().css(attribute,[value])
         */
         vendorCss: function (attribute, value, obj) {
+            this.css(attribute.toLowerCase(),value,obj);
             return this.css($.feat.cssPrefix + attribute, value, obj);
         },
         /**
@@ -45,7 +49,7 @@
          * @title $().vendorCss(value)
          */
         cssTranslate: function (val) {
-            return this.vendorCss("Transform", "translate" + $.feat.cssTransformStart + val + $.feat.cssTransformEnd);
+            this.vendorCss("Transform", "translate" + $.feat.cssTransformStart + val + $.feat.cssTransformEnd);
         },
         /**
          * Gets the computed style of CSS values
@@ -67,11 +71,8 @@
                 classList = classList.replace(classRE(cname), " ");
             };
             for (var i = 0; i < this.length; i++) {
-                if (name == nundefined) {
-                    this[i].className = newName;
-                    continue;
-                }
                 var classList = this[i].className;
+
                 name.split(/\s+/g).concat(newName.split(/\s+/g)).forEach(replaceClassFn);
                 classList = classList.trim();
                 if (classList.length > 0) {
@@ -84,12 +85,13 @@
     });
     function detectUA($, userAgent) {
         $.os = {};
+
         $.os.webkit = userAgent.match(/WebKit\/([\d.]+)/) ? true : false;
         $.os.android = userAgent.match(/(Android)\s+([\d.]+)/) || userAgent.match(/Silk-Accelerated/) ? true : false;
         $.os.androidICS = $.os.android && userAgent.match(/(Android)\s4/) ? true : false;
         $.os.ipad = userAgent.match(/(iPad).*OS\s([\d_]+)/) ? true : false;
         $.os.iphone = !$.os.ipad && userAgent.match(/(iPhone\sOS)\s([\d_]+)/) ? true : false;
-        $.os.ios7 = ($.os.ipad||$.os.iphone)&&userAgent.match(/7_/)||($.os.ipad||$.os.iphone)&&userAgent.match(/8_/) ? true : false;
+        $.os.ios7 = ($.os.ipad||$.os.iphone)&&(userAgent.match(/7_/)||userAgent.match(/8_/)) ? true : false;
         $.os.webos = userAgent.match(/(webOS|hpwOS)[\s\/]([\d.]+)/) ? true : false;
         $.os.touchpad = $.os.webos && userAgent.match(/TouchPad/) ? true : false;
         $.os.ios = $.os.ipad || $.os.iphone;
@@ -104,19 +106,28 @@
         $.os.tizen = userAgent.match(/Tizen/i)?true:false;
         $.os.supportsTouch = ((window.DocumentTouch && document instanceof window.DocumentTouch) || "ontouchstart" in window);
         $.os.kindle=userAgent.match(/Silk-Accelerated/)?true:false;
-        if($.os.ios) {
-            if(Promise&&Promise.toString().indexOf("native")!==-1)
-                $.os.ios7=true;
-        }
         //features
         $.feat = {};
-        var head = document.documentElement.getElementsByTagName("head")[0];
-        $.feat.nativeTouchScroll = typeof(head.style["-webkit-overflow-scrolling"]) !== "undefined" && ($.os.ios||$.os.blackberry10);
-        $.feat.cssPrefix = $.os.webkit ? "Webkit" : $.os.fennec ? "Moz" : $.os.ie ? "ms" : $.os.opera ? "O" : "";
+
         $.feat.cssTransformStart = !$.os.opera ? "3d(" : "(";
         $.feat.cssTransformEnd = !$.os.opera ? ",0)" : ")";
         if ($.os.android && !$.os.webkit)
             $.os.android = false;
+
+
+        //IE tries to be webkit
+        if(userAgent.match(/IEMobile/i)){
+            $.each($.os,function(ind){
+                $.os[ind]=false;
+            });
+            $.os.ie=true;
+            $.os.ieTouch=true;
+        }
+        var items=["Webkit","Moz","ms","O"];
+        for(var j=0;j<items.length;j++){
+            if(document.documentElement.style[items[j]+"Transform"]==="")
+                $.feat.cssPrefix=items[j];
+        }
 
     }
 
@@ -147,11 +158,11 @@
     $.getCssMatrix = function(ele) {
         if ($.is$(ele)) ele = ele.get(0);
 
-        var matrixFn = window.WebKitCSSMatrix || window.MSCSSMatrix;
+        var MatrixFN = window.WebKitCSSMatrix || window.MSCSSMatrix;
 
         if (ele === nundefined) {
-            if (matrixFn) {
-                return new matrixFn();
+            if (MatrixFN) {
+                return new MatrixFN();
             }
             else {
                 return {
@@ -171,8 +182,8 @@
                         computedStyle.transform ||
                         computedStyle[$.feat.cssPrefix + "Transform"];
 
-        if (matrixFn)
-            return new matrixFn(transform);
+        if (MatrixFN)
+            return new MatrixFN(transform);
         else if (transform) {
             //fake css matrix
             var mat = transform.replace(/[^0-9\-.,]/g, "").split(",");
@@ -235,7 +246,7 @@
         return f;
     };
     /**
-     * $.query  - a faster alertnative to $("div");
+     * $.query  - a no longer faster alertnative to $("div") (App Framework was faster)
       ```
       $.query(".panel");
       ```
@@ -253,196 +264,45 @@
         }
     };
 
-    /* The following are for events on objects */
-    /**
-     * Bind an event to an object instead of a DOM Node
-       ```
-       $.bind(this,'event',function(){});
-       ```
-     * @param {Object} object
-     * @param {String} event name
-     * @param {Function} function to execute
-     * @title $.bind(object,event,function);
-     */
-    $.bind = function (obj, ev, f) {
-        if (!obj.__events) obj.__events = {};
-        if (!$.isArray(ev)) ev = [ev];
-        for (var i = 0; i < ev.length; i++) {
-            if (!obj.__events[ev[i]]) obj.__events[ev[i]] = [];
-            obj.__events[ev[i]].push(f);
-        }
-    };
-
-    /**
-     * Trigger an event to an object instead of a DOM Node
-       ```
-       $.trigger(this,'event',arguments);
-       ```
-     * @param {Object} object
-     * @param {String} event name
-     * @param {Array} arguments
-     * @title $.trigger(object,event,argments);
-     */
-    $.trigger = function (obj, ev, args) {
-        var ret = true;
-        if (!obj.__events) return ret;
-        if (!$.isArray(ev)) ev = [ev];
-        if (!$.isArray(args)) args = [];
-        for (var i = 0; i < ev.length; i++) {
-            if (obj.__events[ev[i]]) {
-                var evts = obj.__events[ev[i]];
-                for (var j = 0; j < evts.length; j++)
-                    if ($.isFunction(evts[j]) && evts[j].apply(obj, args) === false)
-                        ret = false;
-            }
-        }
-        return ret;
-    };
-    /**
-     * Unbind an event to an object instead of a DOM Node
-       ```
-       $.unbind(this,'event',function(){});
-       ```
-     * @param {Object} object
-     * @param {String} event name
-     * @param {Function} function to execute
-     * @title $.unbind(object,event,function);
-     */
-    $.unbind = function (obj, ev, f) {
-        if (!obj.__events) return;
-        if(ev==nundefined) {
-            delete obj.__events;
-            return;
-        }
-        if (!$.isArray(ev)) ev = [ev];
-        for (var i = 0; i < ev.length; i++) {
-            if (obj.__events[ev[i]]) {
-                var evts = obj.__events[ev[i]];
-                for (var j = 0; j < evts.length; j++) {
-                    if (f ==nundefined)
-                        delete evts[j];
-                    if (evts[j] === f) {
-                        evts.splice(j, 1);
-                        break;
-                    }
-                }
-            }
-        }
-    };
-    $.cleanUpContent = function(){};
-
     $.isObject = function (obj) {
         return typeof obj === "object";
     };
 
-    $.asap = function (fn, context, args) {
-        if (!$.isFunction(fn)) throw "$.asap - argument is not a valid function";
-        setTimeout(function(){
-            fn.apply(context,args);
-        });
-    };
-
-    /**
-     * this function executes javascript in HTML.
-       ```
-       $.parseJS(content)
-       ```
-    * @param {String|DOM} content
-    * @title $.parseJS(content);
-    */
-    var remoteJSPages = {};
-    $.parseJS = function (div) {
-        if (!div)
-            return;
-        if (typeof (div) === "string") {
-            var elem = document.createElement("div");
-            if(isWin8){
-                MSApp.execUnsafeLocalFunction(function(){
-                    elem.innerHTML = div;
-                });
-            }
-            else
-                elem.innerHTML = div;
-
-            div = elem;
-        }
-        var scripts = div.getElementsByTagName("script");
-        div = null;
-        for (var i = 0; i < scripts.length; i++) {
-            if (scripts[i].src.length > 0 && !remoteJSPages[scripts[i].src]&&!isWin8) {
-                var doc = document.createElement("script");
-                doc.type = scripts[i].type;
-                doc.src = scripts[i].src;
-                document.getElementsByTagName("head")[0].appendChild(doc);
-                remoteJSPages[scripts[i].src] = 1;
-                doc = null;
-            } else {
-                window["eval"](scripts[i].innerHTML);
-            }
-        }
-    };
 
     $.is$ = function (obj) {
         return obj instanceof $;
     };
-    $.jsonP = function(options) {
-        if (isWin8) {
-            options.type = "get";
-            options.dataType = null;
-            return $.get(options);
-        }
-        var callbackName = "jsonp_callback" + (++_jsonPID);
-        var abortTimeout = "",
-            context, callback;
-        var script = document.createElement("script");
-        window[callbackName] = function(data) {
-            clearTimeout(abortTimeout);
-            $(script).remove();
-            delete window[callbackName];
-            options.success.call(context, data);
-        };
-        if (options.url.indexOf("callback=?") !== -1) {
-            script.src = options.url.replace(/=\?/, "=" + callbackName);
-        } else {
-            callback = options.jsonp ? options.jsonp : "callback";
-            if (options.url.indexOf("?") === -1) {
-                options.url += ("?" + callback + "=" + callbackName);
-            }
-            else {
-                options.url += ("&" + callback + "=" + callbackName);
-            }
-            script.src = options.url;
-        }
-        if (options.error) {
-            script.onerror = function() {
-                clearTimeout(abortTimeout);
-                options.error.call(context, "", "error");
-            };
-        }
-        $("head").append(script);
-        if (options.timeout > 0)
-            abortTimeout = setTimeout(function() {
-                options.error.call(context, "", "timeout");
-            }, options.timeout);
-        return {};
-    };
-
     //Shim to put touch events on the jQuery special event
 
     window.$afm=$;
 
+    $.feat.TouchList=function(){
+        this.length=0;
+    };
 
-    if (!window.numOnly) {
-        window.numOnly = function numOnly(val) {
-            if (val ===undefined || val === "") return 0;
-            if (isNaN(parseFloat(val))) {
-                if (val.replace) {
-                    val = val.replace(/[^0-9.-]/g, "");
-                } else return 0;
-            }
-            return parseFloat(val);
-        };
-    }
+    $.feat.TouchList.prototype = {
+        item:function(ind){
+            return this[ind];
+        },
+        _add:function(touch){
+            this[this.length]=touch;
+            this.length++;
+        }
+    };
+    var identifier=1000;
+    $.feat.Touch = function() {
+        this.identifier=identifier++;
+    };
+
+    $.feat.Touch.prototype = {
+        "clientX":0,
+        "clientY":0,
+        "screenX":0,
+        "screenY":0,
+        "pageX":0,
+        "pageY":0,
+        "identifier":0
+    };
 
 })(jQuery,window);
 
