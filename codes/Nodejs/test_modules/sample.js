3b211c7c445366a73c25770de45d164830478c42
flyingdeep <flyingdeep@163.com>
2015-03-11 22:48:30 +0800

    OSS

--- codes/Nodejs/test_modules/sample.js
+++ codes/Nodejs/test_modules/sample.js
@@ -1,7 +1,8 @@
 
 var crypto=require("crypto");
-var policy ='{"expiration":"2015-12-01T12:00:00.000Z","conditions":[[“content-length-range”, 1, 4000]]}';
+var policy ='{"expiration":"2015-12-01T12:00:00.000Z","conditions":[["content-length-range",1,5242880]]}';
 var base64policy = (new Buffer(policy)).toString("base64");
-var key = "glMswMHjOFGZRlhz";
+console.log(base64policy);
+var key = "SLiVEsGzg57ASofurBc0P4RdA1IsjW";
 var result = crypto.createHmac("sha1", key).update(base64policy).digest().toString("base64");
 console.log(result);
