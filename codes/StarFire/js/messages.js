90dfb04aa7756e989952b6b7315dd83d2a14aa40
flyingdeep <flyingdeep@163.com>
2015-04-12 15:03:49 +0800

    UI developing

--- codes/StarFire/js/messages.js
+++ codes/StarFire/js/messages.js
@@ -1,13 +1,16 @@
 var hint_Message =
 {
    STAND_CREATION_SET_POINT_HINT : "点击地图任意处以设定摊位位置",
-    STAND_IMAGE_UPLOAD_SIZE_EXCEED : "所选图片大于2M，请重新选择。"
+    STAND_IMAGE_UPLOAD_SIZE_EXCEED : "所选图片大于2M，请重新选择。",
+    STAND_IMAGE_UPLOAD_CANCEL : "图片上传已经取消",
+    STAND_IMAGE_UPLOAD_FAIL: "上传失败（可能是网络原因）"
 
 };
 
 var normal_Text =
 {
     STAND_CONFIRM_POINT : "是否设定摊位位置？",
+    STAND_UPLOADING: "照片上传中...",
     YES:"确定",
     NO:"取消"
 
